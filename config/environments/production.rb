d7f62202c01cd31b23b2da92abea654cdec18a38
Fernando Garcia <fernandogarcia@MacBook-Pro-2.local>
2020-10-09 16:15:47 +0200

    bug solved

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => user_name
+    :user_name            => user_name,
     :password             => gmail_pass,
     :authentication       => "plain",
     :enable_starttls_auto => true
