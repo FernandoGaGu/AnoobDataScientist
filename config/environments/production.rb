14ff3c00803f8763e71ded7959f1ff91efe29a8f
Fernando Garcia <fernandogarcia@MacBook-Pro-2.local>
2020-10-09 17:08:16 +0200

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Mail configuration
   config.action_mailer.delivery_method = :smtp
-  host = 'example.com' #replace with your own url  <---------------------------------------- REPLACE
+  host = 'anoobdatascientist.herokuapp.com' 
   config.action_mailer.default_url_options = { host: host }
 
   user_name = "a.noob.data.scientist.ca@gmail.com"
@@ -104,7 +104,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  config.active_record.dump_schema_after_migration = true
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
